d6fe6474d08004309e6105016e2759fb177c11be
SMHS-519-13 <SMHS-519-13@SMHS-519-13.smhsrop.lab>
2019-11-14 16:33:55 -0800

    Keith is awesome

--- src/_03_remarkable/Remarkable.java
+++ src/_03_remarkable/Remarkable.java
@@ -8,11 +8,25 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-
+String jon = "Jon you are pretty cool and a good person to have a laugh with!";
+String ian = "Ian I like how nice you are";
+String lok = "Lok you are so smart!";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
+String name = JOptionPane.showInputDialog(null, "What is your name?");
 
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
+if (name.equalsIgnoreCase("ian")) {
+	JOptionPane.showMessageDialog(null, ian);
+}
+if (name.equalsIgnoreCase("jon")) {
+	JOptionPane.showMessageDialog(null, jon);
+}
+if (name.equalsIgnoreCase("lok")) {
+	JOptionPane.showMessageDialog(null, lok);
+}
+else {
+	JOptionPane.showMessageDialog(null, "You are very cool " + name);
+}
 	}
 }
 
--- src/_04_secret_message_box/SecretMessageBox.java
+++ src/_04_secret_message_box/SecretMessageBox.java
@@ -17,7 +17,18 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
-	
+	public static void main(String[] args) {
+		String password = "password";
+		String message = "Who's Joe?";
+		
+		String secret = JOptionPane.showInputDialog(null, "Enter the password to see the secret message");
+		if (secret.equalsIgnoreCase(password)) {
+			JOptionPane.showMessageDialog(null, "Secret message: " + message);
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "INTRUDER!");
+		}
+	}
 	// 1. Set a password in a String variable
 	
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
