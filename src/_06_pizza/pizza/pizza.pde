50f856a5beee078f1c6c1bbe9bf3f19aa01ea96f
SMHS-519-13 <SMHS-519-13@SMHS-519-13.smhsrop.lab>
2019-11-21 16:33:32 -0800

    hi

--- src/_06_pizza/pizza/pizza.pde
+++ src/_06_pizza/pizza/pizza.pde
@@ -1,7 +1,21 @@
 
 void setup() {
-    
+   size(500,500); 
+  
+  
+  
 }
 void draw() {
-    
+    fill(#FFEEA7);
+    ellipse(200, 150, 200, 200);
+    fill(#CE4549);
+    ellipse(200, 150, 180, 180);
+    fill(#F0E8CE);
+    ellipse(200, 150, 170, 170);
+    PImage pepperoni = loadImage ("download.jpg");
+     pepperoni.resize(50,50);
+    image(pepperoni, 175, 125);
+     image(HeroFinal-69.jpg, 150, 100);
+
+   
 }
--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _07_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,15 +12,26 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score = 1;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String answer = JOptionPane.showInputDialog(null, "What can travel the world but stay in the corner?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if (answer .equals("stamp")) {
+	JOptionPane.showMessageDialog(null, "Correct! Your score is " + score++);
+	
+}
+else {
+	JOptionPane.showMessageDialog(null, "Incorrect. The answer is  a stamp. Score: " + score);
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+String answer2 = JOptionPane.showInputDialog(null, "What has hands but can not clap?");
 		// 6. Add some more riddles
-
+if (answer2 .equals("clock")) {
+	JOptionPane.showMessageDialog(null, "Right! Score: " + score++);
+}
+else {
+	JOptionPane.showMessageDialog(null, "Wrong! The answer is a clock.");
+}
 		// 2. Make a pop up to show the score.
 		
 	}
