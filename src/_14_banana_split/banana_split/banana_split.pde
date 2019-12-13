3bf32202b73322c903bba171c9108a9d68c19470
SMHS-519-13 <SMHS-519-13@SMHS-519-13.smhsrop.lab>
2019-12-12 16:35:25 -0800

    work

---
+++ src/_14_banana_split/banana_split/banana_split.pde
@@ -0,0 +1,13 @@
+void setup(){
+  size(400,400);
+  background(#EF00F0);
+}
+void draw(){
+  for(int i=0; i<3; i++){
+    text("ice cream",100, 200+i*20);
+    
+}
+for(int k= 0; k<3; k++){
+  text("banana", 100, 270+k*20);
+}
+}
--- src/badger.java
+++ src/badger.java
@@ -3,36 +3,49 @@ import javax.swing.JOptionPane;
 public class badger {
 	
 public static void main(String[] args) {
-	for (int i = 0; i < 5; i++) {
-		JOptionPane.showMessageDialog(null, "Badger, badger, badger");
+	for (int k = 0; k < 9; k++) {
+		
+	for (int i = 0; i < 2; i++) {
+		JOptionPane.showMessageDialog(null, "Badger");
 	}
+		for (int j = 0; j < 2; j++) {
+			JOptionPane.showMessageDialog(null, "Mushroom");
 	
+		}
+		if (k ==3 || k==8) {
+		
+			for (int i = 0; i < 2; i++) {
+				JOptionPane.showMessageDialog(null, "Snake");
+			}
+		}
 	
+		
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	}
 }
-	
-	
 }
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+
